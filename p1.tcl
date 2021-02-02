bfec9e504ee2cb6d953d077b4ded23f45cdde352
thealpha07 <56728030+thealpha07@users.noreply.github.com>
2021-02-03 02:11:17 +0530

    Good_luck

---
+++ p1.tcl
@@ -0,0 +1,87 @@
+# This script is created by NSG2 beta1
+# <http://wushoupong.googlepages.com/nsg>
+
+#===================================
+#     Simulation parameters setup
+#===================================
+set val(stop)   10.0                         ;# time of simulation end
+
+#===================================
+#        Initialization        
+#===================================
+#Create a ns simulator
+set ns [new Simulator]
+
+#Open the NS trace file
+set tracefile [open p1.tr w]
+$ns trace-all $tracefile
+
+#Open the NAM trace file
+set namfile [open p1.nam w]
+$ns namtrace-all $namfile
+
+#===================================
+#        Nodes Definition        
+#===================================
+#Create 3 nodes
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+#===================================
+#        Links Definition        
+#===================================
+#Createlinks between nodes
+$ns duplex-link $n0 $n1 10.0Mb 10ms DropTail
+$ns queue-limit $n0 $n1 10
+$ns duplex-link $n1 $n2 0.1Mb 10ms DropTail
+$ns queue-limit $n1 $n2 10
+
+#Give node position (for NAM)
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n1 $n2 orient right
+
+#===================================
+#        Agents Definition        
+#===================================
+#Setup a UDP connection
+set udp0 [new Agent/UDP]
+$ns attach-agent $n0 $udp0
+set null1 [new Agent/Null]
+$ns attach-agent $n2 $null1
+$ns connect $udp0 $null1
+$udp0 set packetSize_ 1500
+
+
+#===================================
+#        Applications Definition        
+#===================================
+#Setup a CBR Application over UDP connection
+set cbr0 [new Application/Traffic/CBR]
+$cbr0 attach-agent $udp0
+$cbr0 set packetSize_ 1000
+$cbr0 set rate_ 1.0Mb
+$cbr0 set random_ null
+$ns at 1.0 "$cbr0 start"
+$ns at 2.0 "$cbr0 stop"
+
+
+#===================================
+#        Termination        
+#===================================
+#Define a 'finish' procedure
+proc finish {} {
+    global ns tracefile namfile
+    $ns flush-trace
+    close $tracefile
+    close $namfile
+    exec nam p1.nam &
+    exit 0
+}
+$ns at $val(stop) "$ns nam-end-wireless $val(stop)"
+$ns at $val(stop) "finish"
+$ns at $val(stop) "puts \"done\" ; $ns halt"
+$ns run
+
+#To check Number of Packets dropped use following command
+#grep -c "^d" p1.tr
